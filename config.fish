101a34b9d1e12b0030251b898003ce6c2c3207ee
Jin Chuang <50049549+jinchuangtw@users.noreply.github.com>
2025-12-05 15:28:09 +0800

    Add files via upload

--- config.fish
+++ config.fish
@@ -8,11 +8,13 @@ fish_config theme choose "Dracula Official"
 export PYENV_ROOT="$HOME/.pyenv"
 if command -v pyenv 1>/dev/null 2>&1
   pyenv init --path | source
+  # pyenv init - | source
+  # pyenv virtualenv-init - | source
 end
 
 # ros settings
-# bass source /opt/ros/noetic/setup.bash
-# bass source ~/Development/catkin_ws/devel/setup.bash
+source /opt/ros/noetic/share/rosbash/rosfish
+bass source ~/Development/catkin_ws/devel/setup.bash
 
 # alias settings
 alias ls='logo-ls'
@@ -22,3 +24,9 @@ alias ll='logo-ls -al'
 alias vim='nvim'
 alias vi='nvim'
 alias v='nvim'
+
+alias cm='mkdir build && cd build && cmake ..'
+alias recm='cd .. && rm -rf build && mkdir build && cd build && cmake ..'
+
+alias iec='sudo tailscale up --exit-node="iec-nas.taild6cb20.ts.net"'
+alias iecc='sudo tailscale down'
