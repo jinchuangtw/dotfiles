e9ee0c5b8c594fb6cf8e17b20a9964b29fb82461
Jin Chuang <50049549+jinchuangtw@users.noreply.github.com>
2025-12-23 20:27:53 +0800

    Update configuration files of fish shell

--- config.fish
+++ config.fish
@@ -4,14 +4,16 @@ end
 
 fish_config theme choose "Dracula Official"
 
-# PATH manage
-fish_add_path /usr/local/nvim-linux64/bin
-fish_add_path /usr/local/.cargo/bin
-fish_add_path /usr/local/cuda-11.3/bin 
-fish_add_path /usr/local/MATLAB/R2022a/bin
-fish_add_path /usr/local/texlive/2024/bin/x86_64-linux
-fish_add_path /usr/local/Telegram
+# ------------------------------------------------------------
+# PATH manage in /usr/local
+fish_add_path -g /usr/local/nvim-linux64/bin
+fish_add_path -g /usr/local/.cargo/bin
+fish_add_path -g /usr/local/cuda-11.3/bin 
+fish_add_path -g /usr/local/MATLAB/R2022a/bin
+fish_add_path -g /usr/local/texlive/2024/bin/x86_64-linux
+fish_add_path -g /usr/local/Telegram
 
+# ------------------------------------------------------------
 # alias settings
 alias ls='logo-ls'
 alias la='logo-ls -A'
@@ -27,19 +29,24 @@ alias recm='cd .. && rm -rf build && mkdir build && cd build && cmake ..'
 alias iec='sudo tailscale up --exit-node="iec-nas.taild6cb20.ts.net"'
 alias iecc='sudo tailscale down'
 
+# ------------------------------------------------------------
 # ros settings
 source /opt/ros/noetic/share/rosbash/rosfish
 bass source ~/Development/catkin_ws/devel/setup.bash
 
+# ------------------------------------------------------------
 # pyenv init
-set -gx PYENV_ROOT "$HOMW/.pyenv"
-# export PYENV_ROOT="$HOME/.pyenv"
-if command -v pyenv 1>/dev/null 2>&1
-  pyenv init --path | source
+set -gx PYENV_ROOT $HOME/.pyenv
+if test -d $PYENV_ROOT/bin
+    fish_add_path -g $PYENV_ROOT/bin
+end
+if type -q pyenv
+    pyenv init - fish | source
 end
 
+# ------------------------------------------------------------
+# conda init
 # !! Contents within this block are managed by 'conda init' !!
-# conda initialize
 if test -f /home/jinchuangtw/miniconda3/bin/conda
     eval /home/jinchuangtw/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 else
@@ -53,3 +60,5 @@ end
 if test -d /home/jinchuangtw/miniconda3/bin
     fish_add_path -m /home/jinchuangtw/miniconda3/bin
 end
+
+# -----------------------------------------------------------
